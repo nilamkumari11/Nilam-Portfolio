a2a9d6e4dd6bfcce6d17b9bfe9a32bfbaa214eaf
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-16 21:30:58 +0530

    all icons added

---
+++ src/components/Icons.jsx
@@ -0,0 +1,35 @@
+import { IoCodeWorkingOutline, IoLogoMicrosoft } from "react-icons/io5";
+import { FaGithub, FaTwitter, FaReact, FaJava, FaCode, FaCertificate, FaStar } from "react-icons/fa";
+import { SiTailwindcss,SiGit, SiFramer, SiC, SiHtml5, SiJavascript, SiNodedotjs, SiBootstrap, SiCss3, SiTypescript, SiPython, SiFigma, SiMysql, SiOracle, SiNextdotjs, SiSpringboot, SiPostgresql, SiExpress } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+
+export const Icons = {
+  mongodb: DiMongodb,
+  github: FaGithub,
+  twitter: FaTwitter,
+  react: FaReact,
+  tailwind: SiTailwindcss,
+  framermotion: SiFramer,
+  bootstrap: SiBootstrap,
+  html: SiHtml5,
+  javascript: SiJavascript,
+  typescript: SiTypescript,
+  css: SiCss3,
+  nodejs: SiNodedotjs,
+  python: SiPython,
+  java: FaJava,
+  git: SiGit,
+  figma: SiFigma,
+  mysql: SiMysql,
+  oracle: SiOracle,
+  nextjs: SiNextdotjs,
+  springboot: SiSpringboot,
+  postgresql: SiPostgresql,
+  express: SiExpress, // added express logo
+  c: SiC, // added C language logo
+  code: FaCode,
+  certificate: FaCertificate,
+  star: FaStar,
+  developer: IoCodeWorkingOutline,
+  microsoft: IoLogoMicrosoft
+};
