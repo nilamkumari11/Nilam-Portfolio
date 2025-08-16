017aa253b4d8fc4e7ee88799f5de02f7c674f7a1
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-16 21:33:38 +0530

    data for orbit added

---
+++ src/components/OrbitingItemsData.jsx
@@ -0,0 +1,13 @@
+import { Icons } from "./Icons";
+
+export const testOrbitingItems = [
+  <Icons.github key="github" className="h-6 w-6" />,
+  <Icons.twitter key="twitter" className="h-6 w-6" />,
+  <Icons.react key="react" className="h-6 w-6" />,
+  <Icons.tailwind key="tailwind" className="h-6 w-6" />,
+  <Icons.html key="html" className="h-6 w-6" />,
+  <Icons.css key="css" className="h-6 w-6" />,
+  <Icons.git key="git" className="h-6 w-6" />,
+  <Icons.nodejs key="nodejs" className="h-6 w-6" />,
+  <Icons.javascript key="javascript" className="h-6 w-6" />
+];
