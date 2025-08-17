c2498b3b71f340b5bd5ec99939e5ebe028b47d72
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-17 20:59:16 +0530

    everything updated here

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Background from "./components/Background";
 import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Education } from "./components/Education";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="relative">
       <Background />
-      <Navbar></Navbar>
+        <div className="relative noto-sans-regular font-mono ">
+          <Navbar></Navbar>
+          <Home id="home"></Home>
+          <About id="about"></About>  
+          <Skills id="skills"></Skills>
+          <Education id="education"></Education>
+          <Projects id="projects"></Projects>
+          <Contact id="contact"></Contact>
+          <Footer></Footer>
+        </div>      
+          
+     
     </div>
   );
 }
