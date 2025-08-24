a8175c23e13669c731865b35251bbe525b179bc8
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-24 22:59:45 +0530

    w fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
-import TransitionWithScale from "./TransitionWithScale"
 
 export const Footer = () => {
     return(
-        <TransitionWithScale>
-            <div className="bg-gray-600/40 backdrop-blur-sm h-16 border border-t-cyan-600 w-full">
-            <div className="flex gap-2 m-4.5 text-xl justify-center items-center">
+        <div className="bg-gray-600/40 backdrop-blur-sm h-16 border border-t-cyan-600 w-full">
+            <div className="flex gap-2 m-4 text-xl justify-center items-center">
                 <p className="text-gray-400">Made with</p> 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
@@ -13,6 +11,5 @@ export const Footer = () => {
                 <p className="text-cyan-500 font-bold text-2xl">NILAM</p>
             </div>
         </div>
-        </TransitionWithScale>
     )
 }
